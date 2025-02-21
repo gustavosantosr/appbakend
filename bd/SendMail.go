e6cd74e551a70dbaff93dacccdbdd19c43f42d44
Mac-usb <mac@MacBook-Pro.local>
2025-02-21 02:14:21 -0500

    ajustes al logueo

--- bd/SendMail.go
+++ bd/SendMail.go
@@ -12,14 +12,14 @@ func SendMail() {
 	m := gomail.NewMessage()
 
 	// Configurar remitente, destinatario y asunto
-	m.SetHeader("From", "tuemail@tudominio.com") // Usa un email verificado en Mandrill
+	m.SetHeader("From", "emaill@drmonkey.co") // Usa un email verificado en Mandrill
 	m.SetHeader("To", "gustavosantosr@gmail.com")
 	m.SetHeader("Subject", "Correo de prueba")
 	m.SetBody("text/html", "<h1>Hola!</h1><p>El codigo de seguridad para acceder al sistema es 111111g.</p>")
 
 	// Configurar el servidor SMTP de Mandrill
-	d := gomail.NewDialer("mail.drmonkey.co", 465, "email@drmonkey.co", "Admin4402!")
-
+	//d := gomail.NewDialer("mail.drmonkey.co", 465, "email@drmonkey.co", "Admin4402!")
+	d := gomail.NewDialer("smtp.mandrillapp.com", 587, "emaill@drmonkey.co", "md-AvpMklQ9xGOqLz1Etast_Q")
 	// Enviar el correo
 	if err := d.DialAndSend(m); err != nil {
 		fmt.Println("Error al enviar el correo:", err)
