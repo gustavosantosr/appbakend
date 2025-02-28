186ba817659f1a3b14184870265922ae9d8bacf0
Mac-usb <mac@MacBook-Pro.local>
2025-02-28 11:39:18 -0500

    ajustes

--- bd/SendMail.go
+++ bd/SendMail.go
@@ -21,7 +21,7 @@ func SendMail() {
 	//d := gomail.NewDialer("mail.drmonkey.co", 465, "email@drmonkey.co", "Admin4402!")
 	//d := gomail.NewDialer("mail.drmonkey.co", 465, "email@drmonkey.co", "Admin4402!")
 	//d := gomail.NewDialer("smtp.mandrillapp.com", 587, "email@drmonkey.co", "md-4A6FoxCfhFM7euMC4-WMyw")
-	d := gomail.NewDialer("email-smtp.eu-west-1.amazonaws.com", 587, "AKIA3HHSGBZVMMVON5WR", "BLsNsqHZ7tEsUmdnsXSFcy+M8cq/Bx3ahMkL+R2nCcce")
+	d := gomail.NewDialer("email-smtp.eu-west-1.amazonaws.com", 465, "AKIA3HHSGBZVMMVON5WR", "BLsNsqHZ7tEsUmdnsXSFcy+M8cq/Bx3ahMkL+R2nCcce")
 	// Enviar el correo
 	if err := d.DialAndSend(m); err != nil {
 		fmt.Println("Error al enviar el correo:", err)
