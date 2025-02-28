210a59a249ed65c0aa77969a8b950aea4c26bdc2
Mac-usb <mac@MacBook-Pro.local>
2025-02-28 11:42:02 -0500

    ajustes

--- bd/SendMail.go
+++ bd/SendMail.go
@@ -1,6 +1,7 @@
 package bd
 
 import (
+	"crypto/tls"
 	"fmt"
 
 	"gopkg.in/gomail.v2"
@@ -21,7 +22,8 @@ func SendMail() {
 	//d := gomail.NewDialer("mail.drmonkey.co", 465, "email@drmonkey.co", "Admin4402!")
 	//d := gomail.NewDialer("mail.drmonkey.co", 465, "email@drmonkey.co", "Admin4402!")
 	//d := gomail.NewDialer("smtp.mandrillapp.com", 587, "email@drmonkey.co", "md-4A6FoxCfhFM7euMC4-WMyw")
-	d := gomail.NewDialer("email-smtp.eu-west-1.amazonaws.com", 465, "AKIA3HHSGBZVMMVON5WR", "BLsNsqHZ7tEsUmdnsXSFcy+M8cq/Bx3ahMkL+R2nCcce")
+	d := gomail.NewDialer("email-smtp.eu-west-1.amazonaws.com", 25, "AKIA3HHSGBZVMMVON5WR", "BLsNsqHZ7tEsUmdnsXSFcy+M8cq/Bx3ahMkL+R2nCcce")
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	// Enviar el correo
 	if err := d.DialAndSend(m); err != nil {
 		fmt.Println("Error al enviar el correo:", err)
