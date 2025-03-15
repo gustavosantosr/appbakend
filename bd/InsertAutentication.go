4d6e26e013249763a3afc2f8d89a506055d17343
Mac-usb <mac@MacBook-Pro.local>
2025-03-15 13:01:17 -0500

    cargando codigos

--- bd/InsertAutentication.go
+++ bd/InsertAutentication.go
@@ -2,6 +2,7 @@ package bd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gustavosantosr/twittor/logger"
 )
@@ -13,20 +14,20 @@ func InsertAutenticacion() (int64, bool, error) {
 
 	stmt, es := Conexion.Prepare("INSERT INTO SYSADM.PS_UCA_AUTHENTICATION_APP (DOCUMENT, CODE, REGISTRATION_DATE, STATE) VALUES (?, ?, ?, ?)")
 	if es != nil {
-		logger.WriteLogger(fmt.Sprintf("Error al preparar la consulta: %+v", es))
+		log.Fatal(fmt.Sprintf("Error al preparar la consulta: %+v", es))
 		return 0, false, es
 	}
 	defer stmt.Close() // Cerrar el statement para evitar fugas de memoria
 
 	result, er := stmt.Exec("80919446", "222222", "2025-02-05T08:19:52-05:00", "1")
 	if er != nil {
-		logger.WriteLogger(fmt.Sprintf("Error al ejecutar la consulta: %+v", er))
+		log.Fatal(fmt.Sprintf("Error al ejecutar la consulta: %+v", er))
 		return 0, false, er
 	}
 
 	resultado, err := result.LastInsertId()
 	if err != nil {
-		logger.WriteLogger(fmt.Sprintf("Error al obtener LastInsertId: %+v", err))
+		log.Fatal(fmt.Sprintf("Error al obtener LastInsertId: %+v", err))
 		return 0, false, err
 	}
 
