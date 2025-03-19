c1a00d2750d8b353ad89438dd27d75d22db92559
Mac-usb <mac@MacBook-Pro.local>
2025-03-19 05:41:52 -0500

    credenciales

--- bd/GetAutenticacion.go
+++ bd/GetAutenticacion.go
@@ -34,7 +34,7 @@ func GetAutenticacion() ([]*models.Autenticacion, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 	//InsertAutenticacion()
-	SendMail()
+	//SendMail()
 
 	query := `
   SELECT * 
