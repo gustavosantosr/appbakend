b8b92a7bd55e1284ff28799b42d35f9519648f99
Mac-usb <mac@MacBook-Pro.local>
2025-03-19 10:17:35 -0500

    ajustes

---
+++ routers/getAutenticacionTotal.go
@@ -0,0 +1,33 @@
+package routers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gustavosantosr/twittor/bd"
+	"github.com/gustavosantosr/twittor/logger"
+)
+
+/*GetAutenticacion Leo los Apoyo */
+func GetAutenticacionTotal(w http.ResponseWriter, r *http.Request) {
+
+	//1053846544
+	respuesta, correcto := bd.GetAutenticacion()
+	//_, respuesta, correcto := bd.InsertAutenticacion()
+	if correcto != nil {
+		logger.WriteLogger(fmt.Sprintf("%+v", correcto.Error()))
+		log.Fatal(fmt.Sprintf("%+v", correcto.Error()))
+		http.Error(w, "Error al leer los Apoyo", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(respuesta)
+}
